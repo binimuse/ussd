5ac300f49617a8692cbf4fe8d7dc2345b8870fbc
binimuse <bini.musema@gmail.com>
2020-07-21 01:10:48 -0700

    first commit

--- server.js
+++ server.js
@@ -31,13 +31,14 @@ app.post('*', (req, res) => {
     res.send(response)
   } else if (text == '2') {
     // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
+    res.status(400).send('Bad request!')
     res.send(response)
   } else if (text == '1*1') {
     // Business logic for first level response
     let accountNumber = 'ACC1001'
     // This is a terminal request. Note how we start the response with END
-    let response = `CON Taxiye Normal	please Choose driver
+    let response = `CON Taxiye Normal
+	please Choose driver
 	1. +251923798638.usr on 10 km
 	2. +251923775330.usr on 30 km`
     res.send(response)
@@ -45,11 +46,18 @@ app.post('*', (req, res) => {
     // This is a second level response where the user selected 1 in the first instance
     let balance = 'NGN 10,000'
     // This is a terminal request. Note how we start the response with END
-    let response = `CON Taxiye Mini	please Choose driver
+    let response = `CON Taxiye Mini
+	please Choose driver
 	1. +251923798638.usr on 10 km
 	2. +251923775330.usr on 30 km`
     res.send(response)
-  } else {
+  } 
+  
+  else if (text == '1*1*1') {
+	  let response = `Confirmation message will be sent 
+								Thank you!!!`
+  }
+  else {
     res.status(400).send('Bad request!')
   }
 })
