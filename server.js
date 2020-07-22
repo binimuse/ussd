9000bb7ee967e87bc0df3847f7b5764377dcc3d6
binimuse <bini.musema@gmail.com>
2020-07-21 23:30:58 -0700

    first commit

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('*', (req, res) => {
   } 
   
   
-  else if (text == '1*1*1*1') {
+  else if (text == '1*1*1') {
 	  let response = `END Confirmation message will be sent 
 								Thank you!!!`
 	   res.send(response)
